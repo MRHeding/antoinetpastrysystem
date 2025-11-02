0df664218d9ad9e4914c69e24d3b8c699af11bdd
Mohammad Rasheed <rasheed121099@gmail.com>
2025-11-02 20:20:29 +0800

    feat(dependencies): initialize Composer setup with Twilio SDK
    - Added composer.lock to lock project dependencies, ensuring consistent installations.
    - Introduced composer.phar for managing PHP dependencies.
    - Generated autoload files for PSR-4 compliance, facilitating class loading.
    - Included Twilio SDK version 7.16.2 for API integration, enhancing communication capabilities.
    - Created installed.json and installed.php to track installed packages and their versions.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'antoinettes-pastries/system',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b166be2e06d95af9730321713eddf5b2af7c34e2',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'antoinettes-pastries/system' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b166be2e06d95af9730321713eddf5b2af7c34e2',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '7.16.2',
+            'version' => '7.16.2.0',
+            'reference' => '02ad214b0cc9fc513bd67df251d54aed8901284f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
